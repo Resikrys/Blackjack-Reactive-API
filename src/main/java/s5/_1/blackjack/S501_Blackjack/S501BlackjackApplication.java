279c861d5988a0c09205d6da4eca07fbfc55a0c7
resikrys <resicris@gmail.com>
2025-10-21 11:17:28 +0200

    first commit

---
+++ src/main/java/s5/_1/blackjack/S501_Blackjack/S501BlackjackApplication.java
@@ -0,0 +1,13 @@
+package s5._1.blackjack.S501_Blackjack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S501BlackjackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S501BlackjackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/s5/_1/blackjack/S501_Blackjack/S501BlackjackApplicationTests.java
@@ -0,0 +1,13 @@
+package s5._1.blackjack.S501_Blackjack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S501BlackjackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
